8a9c05a0ee62fc69500eb64ce36a3e8ffdf73394
alex <alex.ngumbau@equitybank.co.ke>
2024-06-09 20:10:10 +0300

    Axtivating the user

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
@@ -17,6 +18,7 @@ def detectUser(user):
         return redirectUrl
 
 def send_verification_email(request, user):
+    from_email = settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
     mail_subject ='Please activate your account'
     message =  render_to_string('accounts/emails/account_verification_email.html', {
@@ -26,5 +28,5 @@ def send_verification_email(request, user):
         'token': default_token_generator.make_token(user),
     })
     to_email = user.email
-    mail =  EmailMessage(mail_subject, message,to=[to_email])
+    mail =  EmailMessage(mail_subject, message, from_email, to=[to_email])
     mail.send()
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,8 @@ from . forms import UserForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -108,7 +110,20 @@ def registerVendor(request):
 
 def activate(request, uidb64, token):
     # Activate the user by setting the is_active status to True
-    return 
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, OverflowError, User.DoesNotExist):
+        user=None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations Your account is activated.')
+        return redirect('myAccount')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('myAccount')
 
 def login(request):
     if request.user.is_authenticated:
