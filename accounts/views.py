c85f3523b58570305ca486570b25f863e4e0ac78
alex <alex.ngumbau@equitybank.co.ke>
2024-06-09 20:40:55 +0300

    Forgot password setup

--- accounts/views.py
+++ accounts/views.py
@@ -172,3 +172,14 @@ def custDashboard(request):
 def vendorDashboard(request):
 
     return render(request, 'accounts/vendorDashboard.html')
+
+
+def forgot_password(request):
+    return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request, uidb64, token):
+    return
+
+def reset_password(request):
+    return render(request, 'accounts/reset_password.html')
