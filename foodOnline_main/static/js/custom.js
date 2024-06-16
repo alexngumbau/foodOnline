b364da014ea6e90c00fcb953a2fa6cf7e5c9b79b
alex <alex.ngumbau@equitybank.co.ke>
2024-06-17 00:33:22 +0300

    Looped through address components and fetched data

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -43,4 +43,26 @@ function onPlaceChanged () {
     });
 
     // loop through the address components and assign other address data
+    for (var i = 0; i<place.address_components.length; i++) {
+        for (var j = 0; j<place.address_components[i].types.length; j++) {
+            // get country
+            if (place.address_components[i].types[j] == 'country' ) {
+                $('#id_country').val(place.address_components[i].long_name);
+            }
+            // get state
+            if (place.address_components[i].types[j] == 'administrative_area_level_1' ) {
+                $('#id_state').val(place.address_components[i].long_name);
+            }
+            // get city
+            if (place.address_components[i].types[j] == 'locality' ) {
+                $('#id_city').val(place.address_components[i].long_name);
+            }
+            // get pincode
+            if (place.address_components[i].types[j] == 'postal_code' ) {
+                $('#id_pin_code').val(place.address_components[i].long_name);
+            } else {
+                $('#id_pin_code').val("");
+            }
+        }
+    }
 }
