de96d65fa531e0ef38b207dd33f964cfe5da3d6f
alex <alex.ngumbau@equitybank.co.ke>
2024-06-20 08:48:46 +0300

    menu builder fooditem front end part done

--- vendor/views.py
+++ vendor/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
-from menu.models import Category
+from menu.models import Category, FoodItem
 from vendor.forms import VendorForm
 from vendor.models import Vendor
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
+from vendor.utils import get_vendor
 # Create your views here.
+
+
 
 @login_required(login_url = 'login')
 @user_passes_test(check_role_vendor)
@@ -42,11 +45,27 @@ def vprofile(request):
     return render(request, 'vendor/vprofile.html', context)
 
 
+@login_required(login_url = 'login')
+@user_passes_test(check_role_vendor)
 def menu_builder(request):
-    vendor = Vendor.objects.get(user=request.user)
+    vendor = get_vendor(request)
     categories = Category.objects.filter(vendor=vendor)
     context = {
         'categories': categories
     }
     return render(request, 'vendor/menu_builder.html', context)
+
+
+@login_required(login_url = 'login')
+@user_passes_test(check_role_vendor)
+def foodItems_by_category (request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    foodItems = FoodItem.objects.filter(vendor=vendor, category=category)
+    print("Food items: ", foodItems)
+    context = {
+        'foodItems' : foodItems,
+        'category' : category,
+    }
+    return render (request, 'vendor/foodItems_by_category.html', context)
 
