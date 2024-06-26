71ed4fe78576b2ee5e93c368de4c1b7b7433016c
alex <alex.ngumbau@equitybank.co.ke>
2024-06-26 08:05:30 +0300

    endor detail page setup -- DONE

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from marketplace import views
 
 urlpatterns = [
-    path('', views.marketplace, name='marketplace')
+    path('', views.marketplace, name='marketplace'),
+    path('<slug:vendor_slug>/', views.vendor_detail, name='vendor_detail'),
 ]
