23bd3f0199674bc4f7d97e668e1960ea7677e28c
alex <alex.ngumbau@equitybank.co.ke>
2024-06-26 09:32:24 +0300

    Add to cart url setup

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -66,3 +66,15 @@ function onPlaceChanged () {
         }
     }
 }
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.add_to_cart').on('click', function(e){
+        e.preventDefault();
+        alert('test123');
+    })
+})
