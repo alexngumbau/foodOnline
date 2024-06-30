63fef79ecd194f4d3e55a4c481d2742d6e564737
alex <alex.ngumbau@equitybank.co.ke>
2024-06-30 14:25:25 +0300

    handled the decrease cart bugs before entering into the cart

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -114,6 +114,7 @@ $(document).ready(function(){
 
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
+        cart_id = $(this).attr('id');
         
 
         $.ajax({
@@ -130,6 +131,11 @@ $(document).ready(function(){
                 } else {
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    if (window.location.pathname == '/cart/') {
+                        removeCartItem(response.qty, cart_id)
+                        checkEmptyCart()
+                    }
                 }
                 
                 
