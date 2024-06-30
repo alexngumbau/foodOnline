3e4b6dd7f647413226d36fa5e34fe0192dc51a97
alex <alex.ngumbau@equitybank.co.ke>
2024-06-30 11:02:07 +0300

    Cart url setup --DONE

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,11 +4,12 @@ from marketplace import views
 
 urlpatterns = [
     path('', views.marketplace, name='marketplace'),
+    
     path('<slug:vendor_slug>/', views.vendor_detail, name='vendor_detail'),
 
     # Add to cart
     path('add_to_cart/<int:food_id>/' ,views.add_to_cart, name='add_to_cart'),
     # Decress cart
     path('decrease_cart/<int:food_id>/' ,views.decrease_cart, name='decrease_cart'),
-
+    
 ]
