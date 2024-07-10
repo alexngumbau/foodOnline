874a4ee51c5fe70f0f86f31bb9ae180c5f9cb11b
alex <alex.ngumbau@equitybank.co.ke>
2024-07-10 08:28:57 +0300

    Show nearby restaurants on Homepage

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse
 
 from vendor.models import Vendor
 
+
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D
+from django.contrib.gis.db.models.functions import Distance
+
 def home(request):
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+
+        pnt = GEOSGeometry('POINT(%s  %s)' %(lng, lat))
+        vendors = Vendor.objects.filter(userProfile__location__distance_lte=(pnt, D(km=10000))
+        ).annotate(distance=Distance("userProfile__location", pnt)).order_by("distance")
+    
+        for v in vendors:
+            v.kms =  round(v.distance.km, 1)
+
+    else:
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
     context = {
         'vendors':vendors,
     }
